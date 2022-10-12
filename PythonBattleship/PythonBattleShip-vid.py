3cd2f243136b384b11aa32459780980d171e2de7
Dom123x456 <dominicedgar06@gmail.com>
2022-10-12 11:31:29 +0000

    added defprint board and letters to numbers a to g

--- PythonBattleship/PythonBattleShip-vid.py
+++ PythonBattleship/PythonBattleShip-vid.py
@@ -1,3 +1,8 @@
 HIDDEN_BOARD = [[" "] * 8 for x in range(8)]
 
 GUESS_BOARD = [[" "] * 8 for i in range(8)]
+
+letters_to_numbers = {'a': 0,'b': 1,'c': 2,'d': 3,'e': 4,'f': 5,'g': 6,'H': 7 }
+
+def print_board(board):
+    pass
