d89c4048078dd40e767ec9a2e0e7e631091fda07
Dom123x456 <dominicedgar06@gmail.com>
2022-10-12 11:36:12 +0000

    re ordered letteres to numbers, put them in caps

--- PythonBattleship/PythonBattleShip-vid.py
+++ PythonBattleship/PythonBattleShip-vid.py
@@ -2,7 +2,16 @@ HIDDEN_BOARD = [[" "] * 8 for x in range(8)]
 
 GUESS_BOARD = [[" "] * 8 for i in range(8)]
 
-letters_to_numbers = {'a': 0,'b': 1,'c': 2,'d': 3,'e': 4,'f': 5,'g': 6,'H': 7 }
+letters_to_numbers = {
+    'A': 0,
+    'B': 1,
+    'C': 2,
+    'D': 3,
+    'E': 4,
+    'F': 5,
+    'G': 6,
+    'H': 7
+} 
 
 def print_board(board):
     pass
