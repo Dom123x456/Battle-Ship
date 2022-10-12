05add9e7f231bf0cfd16f1b18e3121048136399f
Dom123x456 <dominicedgar06@gmail.com>
2022-10-12 12:14:26 +0000

    added legend at top of code for game rules

--- PythonBattleship/PythonBattleShip-vid.py
+++ PythonBattleship/PythonBattleShip-vid.py
@@ -1,3 +1,9 @@
+#Legend
+# X for placement of ship and hit of ballteship
+#' ' for space that is avalible
+#'#' for hit battle ship
+
+
 from random import randint
 
 HIDDEN_BOARD = [[" "] * 8 for x in range(8)]
